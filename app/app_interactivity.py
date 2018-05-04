7e33905db0686466bc96f4eb47d1ac21a5529c8f
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-05-05 01:52:34 +0900

    done tutorial

---
+++ app/app_interactivity.py
@@ -0,0 +1,27 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    dcc.Input(id='my-id', value='initial value', type='text'),
+    html.Div(id='my-div',
+    style={
+        'marginTop': '24px'
+    })
+])
+
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=5000, host='0.0.0.0')
