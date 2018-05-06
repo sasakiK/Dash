849d74288a76773a3f94b1ef232d6c6d9eb19489
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-05-06 16:53:42 +0900

    add googlefont and change styl.

--- app/app.py
+++ app/app.py
@@ -1,32 +1,92 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+import pandas as pd
+import plotly.graph_objs as go
 
 app = dash.Dash()
 
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'cb5392c35661370d95f300086accea51/raw/'
+    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+    'indicators.csv')
+
+available_indicators = df['Indicator Name'].unique()
+
 app.layout = html.Div([
-    dcc.Input(id='input-1-state', type='text', value='Montréal'),
-    dcc.Input(id='input-2-state', type='text', value='Canada'),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.Div(id='output-state')
-])
+    html.H1(['Big Data Analytics option task'],
+    style={'margin-left': '10%'}),
+    html.Div([
 
+        html.Div([
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '80%', 'position': 'auto', 'margin': 'auto'}),
 
-@app.callback(Output('output-state', 'children'),
-              [Input('submit-button', 'n_clicks')],
-              [State('input-1-state', 'value'),
-               State('input-2-state', 'value')])
-def update_output(n_clicks, input1, input2):
-    return u'''
-        The Button has been pressed {} times,
-        Input 1 is "{}",
-        and Input 2 is "{}"
-    '''.format(n_clicks, input1, input2)
+        html.Div([
+            dcc.Graph(id='indicator-graphic'),
 
+            dcc.Slider(
+                id='year--slider',
+                min=df['Year'].min(),
+                max=df['Year'].max(),
+                value=df['Year'].max(),
+                step=None,
+                marks={str(year): str(year) for year in df['Year'].unique()}
+            )
+        ],
+        style={'width': '80%', 'position': 'auto', 'margin': 'auto'})
+        ])
+],
+style={'position': 'relative', 'width': '100%', 'margin-bottom': '20px', 'font-family': 'Dosis'})
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+@app.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('xaxis-type', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+def update_graph(xaxis_type, year_value):
+    dff = df[df['Year'] == year_value]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+external_css = [
+    # dash stylesheet
+    'https://fonts.googleapis.com/css?family=Raleway',
+    'https://codepen.io/chriddyp/pen/bWLwgP.css'
+]
+for css in external_css:
+    app.css.append_css({'external_url': css})
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=5000, host='0.0.0.0')
